66e59a3f7aa3e339b04f435dec3abb43d8b4f1a9
petitsurfeur <bob@petitsurfeur.net>
2017-06-10 17:12:55 +0200

    Create install.sh

--- exim4/install.sh
+++ exim4/install.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+echo "Installation d'Exim4"
 apt install exim4 --yes
 
 if [ ! -f /etc/exim4/update-exim4.conf.conf.SAVE ]; then
  cp /etc/exim4/update-exim4.conf.conf /etc/exim4/update-exim4.conf.conf.SAVE
 fi
 
+echo "Copie du fichier de conf"
 cp update-exim4.conf.conf /etc/exim4/
 
+echo "Mise-à-jour du fichier /etc/mailname"
 echo rhea.cybtech.net > /etc/mailname
 
+echo "Redémarrage d'Exim4"
 service exim4 restart
 
+echo "Envoi d'un mail de test"
 echo "Ceci est un mail de test." | mail -s Test_Envoi_Mail fremaud@numericable.com
