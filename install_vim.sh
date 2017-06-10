cc3d0aa01ba652ee0d17199f6b29b0ba16172930
petitsurfeur <bob@petitsurfeur.net>
2017-06-10 14:47:34 +0200

    Delete install_vim.sh

--- install_vim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Author: petitsurfeur
-#This Script is a part of cybit-script
-#More informations: 
-# Install and configure vim
-apt update
-apt remove vim-tiny --force-yes && apt install vim --force-yes
