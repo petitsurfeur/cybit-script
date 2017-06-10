b161d81930817b31fe27799c297e8f1aa417303e
petitsurfeur <bob@petitsurfeur.net>
2017-06-10 19:24:18 +0200

    Create install.sh

--- fail2ban/install.sh
+++ fail2ban/install.sh
@@ -8,6 +8,16 @@ if [ ! -f /etc/fail2ban/jail.conf.SAVE ]; then
  cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.conf.SAVE
 fi
 
-sed -i -e 's/^#*Port.*/Port 2022/' '/etc/ssh/sshd_config'
-sed -i -e 's/^#*ignoreip =.*/ignoreip = 127.0.0.1/8 home.cybtech.net/' '/etc/fail2ban/jail.conf'
+sed -i -e 's/^#*ignoreip.*/ignoreip = 127.0.0.1\/8 XXX.cybtech.net/' '/etc/fail2ban/jail.conf'
 sed -i -e 's/^#*bantime =.*/bantime = 864000/' '/etc/fail2ban/jail.conf'
+sed -i -e 's/^#*findtime =.*/findtime = 6000/' '/etc/fail2ban/jail.conf'
+sed -i -e 's/^#*maxretry =.*/maxretry = 3/' '/etc/fail2ban/jail.conf'
+sed -i -e 's/^#*destemail =.*/destemail = XXX@cybtech.net/' '/etc/fail2ban/jail.conf'
+sed -i -e 's/^#*sendername =.*/sendername = Fail2Ban - XXX/' '/etc/fail2ban/jail.conf'
+sed -i -e 's/^#*sender =.*/sender = XXX@cybtech.net/' '/etc/fail2ban/jail.conf'
+sed -i -e 's/^#*action =.*/action = %(action_mwl)s/' '/etc/fail2ban/jail.conf'
+sed -i -e 's/^#*port = ssh/port = 22,2022/' '/etc/fail2ban/jail.conf'
+
+
+echo " Copie des fichiers dans /
+echo "Configuration de jail.local"
