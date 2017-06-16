f6f87c967affd511512c52ddbd1e158df1dd02d3
petitsurfeur <bob@petitsurfeur.net>
2017-06-16 19:50:42 +0200

    Validation script Vim

--- ssh/ssh_install.sh
+++ ssh/ssh_install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#Author: petitsurfeur
-#This Script is a part of cybit-script
-#More informations:
-# Configure SSH
+set -e
+
 #apt update
 apt install openssh-server
 
@@ -13,7 +11,11 @@ fi
 sed -i -e 's/^#*Port.*/Port 2022/' '/etc/ssh/sshd_config'
 sed -i -e 's/^#*LoginGraceTime.*/LoginGraceTime 30/' '/etc/ssh/sshd_config'
 sed -i -e 's/^#*PermitRootLogin.*/PermitRootLogin no/' '/etc/ssh/sshd_config'
-sed -i 's/^#*MaxStartups.*$/MaxStartups 3:50:10/g' /etc/ssh/sshd_config
-
+sed -i '/PermitRootLogin no/a\AllowUsers' '/etc/ssh/sshd_config'
+sed 's/^#*MaxStartups.*$/MaxStartups 3:50:10/g' /'etc/ssh/sshd_config'
 
 #service sshd restart
+
+echo ""
+echo "Penser a ajouter AllowUsers"
+echo ""
