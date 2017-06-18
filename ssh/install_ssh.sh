db550f7149b71e11082e5f206b1c97be3116cab8
petitsurfeur <bob@petitsurfeur.net>
2017-06-18 02:19:46 +0200

    SSH : correction MaxStartup + service

--- ssh/install_ssh.sh
+++ ssh/install_ssh.sh
@@ -16,11 +16,11 @@ sed -i -e 's/^#*Port.*/Port 2022/' '/etc/ssh/sshd_config'
 sed -i -e 's/^#*LoginGraceTime.*/LoginGraceTime 30/' '/etc/ssh/sshd_config'
 sed -i -e 's/^#*PermitRootLogin.*/PermitRootLogin no/' '/etc/ssh/sshd_config'
 sed -i '/PermitRootLogin no/a\AllowUsers' '/etc/ssh/sshd_config'
-sed 's/^#*MaxStartups.*$/MaxStartups 3:50:10/g' /'etc/ssh/sshd_config'
+sed -i -e 's/^#*MaxStartups.*$/MaxStartups 3:50:10/g' /'etc/ssh/sshd_config'
 
 echo ""
 echo "### Redemarrage du service"
-service sshd restart
+service ssh restart
 
 echo ""
 echo "### Penser a modifier AllowUsers"
