fafa58d499b2175cb729f87fd4867ea5c9934bc1
petitsurfeur <root@cyb.pw>
2017-07-09 22:14:05 +0200

    correction

--- openvpn/openvpn_configure_ufw.sh
+++ openvpn/openvpn_configure_ufw.sh
@@ -10,6 +10,6 @@ echo ""
 echo -e "${GREEN}### Configuration Reseau pour OpenVPN${NOCOLOR}"
 
 #sed -i -e 's/^#*ufw allow in openvpn\/tcp/ufw allow in openvpn\/tcp/' '/etc/init.d/FirewallRules'
-#sed -i -e 's/^#*net.ipv4.ip.forward=1/net.ipv4.ip.forward=1/' '/etc/sysctl.conf'
+#sed -i -e 's/^#*net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' '/etc/sysctl.conf'
 
 sed -i -e '/*nat/ r before.rules.sed' '/etc/ufw/before.rules'
