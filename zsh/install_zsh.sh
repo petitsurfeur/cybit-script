e9162ed8937faceeef3866e676498e8931e10f8e
petitsurfeur <root@srv-ubuntu-02.cyb.pw>
2020-05-31 10:27:01 +0200

    correction

--- zsh/install_zsh.sh
+++ zsh/install_zsh.sh
@@ -29,15 +29,15 @@ read -p "Voulez-vous configurer le theme [O/n] ? " zsh_theme_choice
     sed -i -e 's/^#plugins=.*/plugins=(git zsh-autosuggestions zsh-syntax-highlighting colored-man-pages)'
     source ~/.zshrc
 
-    cat << 'EOF' > ~/.zshrc
-    alias ls='ls --color=auto'
-    alias ll='ls -alFh'
-    
-    #LS_COLORS='di=34;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
-    LS_COLORS='di=1;104:fi=0:ln=31:pi=5:so=5:bd=5:cd=5:or=31:mi=0:ex=35:*.rpm=90'
-    export LS_COLORS
-    EOF
-  fi
+cat << 'EOF' > ~/.zshrc
+alias ls='ls --color=auto'
+alias ll='ls -alFh'
+
+#LS_COLORS='di=34;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
+LS_COLORS='di=1;104:fi=0:ln=31:pi=5:so=5:bd=5:cd=5:or=31:mi=0:ex=35:*.rpm=90'
+export LS_COLORS
+EOF
+fi
 
 read -p "autre compte a appliquer: " zsh_otheraccount
 cp ~/.zshrc /$zsh_otheraccount && chown $zsh_otheraccount:$zsh_otheraccount $zsh_otheraccount/.zshrc
